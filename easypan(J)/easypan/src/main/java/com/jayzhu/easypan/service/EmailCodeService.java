79b62dd4a81474704e6364c87620400f18529c2d
Dyi <110678873+Dai-rj@users.noreply.github.com>
2025-03-13 15:38:31 +0800

    Delete easypan(J) directory

--- easypan(J)/easypan/src/main/java/com/jayzhu/easypan/service/EmailCodeService.java
+++
@@ -1,9 +0,0 @@
-package com.jayzhu.easypan.service;
-
-import com.jayzhu.easypan.exception.BusinessException;
-
-public interface EmailCodeService {
-    void sendEmailCode(String email,Integer type) throws BusinessException;
-
-    void checkCode(String email,String code);
-}
